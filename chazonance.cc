ba61f40359cd840701dfeea260eb5a3e6433e39f
carrien <carrien@bu.edu>
2019-10-27 14:13:21 -0500

    Equalize gain

--- chazonance.cc
+++ chazonance.cc
@@ -11,26 +11,12 @@
 
 using namespace std;
 
-template <typename Signal>
-tuple<float, float, size_t> amplitude( const Signal & samples )
+ComplexSignal time2frequency( RealSignal & samples )
 {
-    if ( samples.empty() ) {
-	throw runtime_error( "samples cannot be empty" );
-    }
-
-    float sum = 0;
-    float peak = -1;
-    size_t peak_location = -1;
-
-    for ( size_t i = 0; i < samples.size(); i++ ) {
-	const float val = norm( samples[ i ] );
-	sum += val;
-	if ( val > peak ) {
-	    peak = val;
-	    peak_location = i;
-	}
-    }
-    return { sqrt( sum / samples.size() ), sqrt( peak ), peak_location };
+    ComplexSignal frequency( samples.size() / 2 + 1 );    
+    FFTPair fft { samples, frequency };
+    fft.time2frequency( samples, frequency );
+    return frequency;
 }
 
 float dB( const float x )
@@ -38,11 +24,61 @@ float dB( const float x )
     return 20 * log10( x );
 }
 
+struct Info
+{
+  struct Stats {
+    double rms_amplitude;
+    double peak_amplitude;
+    double peak_at;
+
+    template <typename Signal>
+    Stats( const Signal & samples, const double length_in_units )
+      : rms_amplitude(), peak_amplitude(), peak_at()
+    {
+      if ( samples.empty() ) {
+	throw runtime_error( "samples cannot be empty" );
+      }
+
+      float sum = 0;
+      float peak = -1;
+      size_t peak_location = -1;
+
+      for ( size_t i = 0; i < samples.size(); i++ ) {
+	const float val = norm( samples[ i ] );
+	sum += val;
+	if ( val > peak ) {
+	  peak = val;
+	  peak_location = i;
+	}
+      }
+
+      rms_amplitude = sqrt( sum / samples.size() );
+      peak_amplitude = sqrt( peak );
+      peak_at = peak_location / length_in_units;
+    }
+  };
+
+  Stats time_domain, frequency_domain;
+
+  Info( RealSignal & samples )
+    : time_domain( samples, float( SAMPLE_RATE ) / samples.size() ),
+      frequency_domain( time2frequency( samples ), MAX_FREQUENCY )
+  {
+    cout << "Time-domain stats: RMS amplitude = " << dB( time_domain.rms_amplitude ) << " dB"
+	 << " and peak amplitude = " << dB( time_domain.peak_amplitude ) << " dB"
+	 << " @ " << time_domain.peak_at << "\n";
+
+    cout << "Freq-domain stats: RMS amplitude = " << dB( frequency_domain.rms_amplitude ) << " dB"
+	 << " and peak amplitude = " << dB( frequency_domain.peak_amplitude ) << " dB"
+	 << " @ " << frequency_domain.peak_at << "\n";
+  }
+};
+
 void program_body( const string & filename )
 {
     WAV wav;
     wav.read_from( filename );
-    wav.samples().resize(wav.samples().size()+SAMPLE_RATE);
+    wav.samples().resize(wav.samples().size()+SAMPLE_RATE*2);
 
     {
 	SoundCard sound_card { "default", "default" };
@@ -55,18 +91,20 @@ void program_body( const string & filename )
 	}
     }
 
-    ComplexSignal frequency( wav.samples().size() / 2 + 1 );
-    FFTPair fft { wav.samples(), frequency };
-
     RealSignal input( wav.samples().size() );
 
     cout << fixed << setprecision( 2 );
 
+    /* get overall stats */
+    Info original_info( wav.samples() );
+    
     for ( unsigned int iter = 0;; iter++ ) {
 	/* write out */
 	wav.write_to( filename + "-generation" + to_string( iter ) + ".wav" );
 
 	/* eliminate frequencies below 20 Hz */
+	ComplexSignal frequency( wav.samples().size() / 2 + 1 );
+	FFTPair fft( wav.samples(), frequency );
 	fft.time2frequency( wav.samples(), frequency );
 	for ( unsigned int i = 0; i < frequency.size(); i++ ) {
 	    const float frequency_in_hertz = i * MAX_FREQUENCY / frequency.size();
@@ -74,21 +112,21 @@ void program_body( const string & filename )
 		frequency[ i ] = 0;
 	    }
 	}
-	auto frequency_save = frequency;
 	fft.frequency2time( frequency, wav.samples() );
 
-	/* find RMS amplitude and peak amplitudes */
-	const auto [ rms, peak, peak_location ] = amplitude( wav.samples() );
-	cout << "Iteration " << iter << ", playing " << wav.samples().size() / float( SAMPLE_RATE )
-	     << " seconds with RMS amplitude = " << dB( rms ) << " dB"
-	     << " and peak amplitude = " << dB( peak ) << " dB"
-	     << " @ " << peak_location / float( SAMPLE_RATE ) << " s.\n";
+	/* get stats */
+	Info before_transform_info( wav.samples() );
 
-	const auto [ rms_freq, peak_freq, peak_location_freq ] = amplitude( frequency_save );
-	cout << "In frequency domain, RMS amplitude = " << dB( rms_freq ) << " dB"
-	     << " and peak amplitude = " << dB( peak_freq ) << " dB"
-	     << " @ " << peak_location_freq * MAX_FREQUENCY / frequency_save.size() << " Hz.\n";
-
+	/* apply equal gain */
+	float gain = original_info.time_domain.rms_amplitude / before_transform_info.time_domain.rms_amplitude;
+	cout << "Applying gain of " << gain << "\n";
+	for ( auto & sample : wav.samples() ) {
+	  sample *= gain;
+	}
+	
+	/* get stats */
+	Info after_transform_info( wav.samples() );
+	
 	/* play and record */
 
 	SoundCard sound_card { "default", "default" };
